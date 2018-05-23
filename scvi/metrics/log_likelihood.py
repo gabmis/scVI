78aba9e8c691b336fc34b2cee6277f6341717071
Edouard360 <mailedouardmehlman@gmail.com>
2018-05-23 13:27:51 -0700

    Remove nan values for LL.
    - Take pytorch softplus implementation (instead of log(1+exp) which leads to overflow)
    - Add eps for safeguards in lgamma
    - Clamp library parameter in Decoder SCVI to around exp(8) ~ 3000 counts per cell

--- scvi/metrics/log_likelihood.py
+++ scvi/metrics/log_likelihood.py
@@ -1,6 +1,7 @@
 """File for computing log likelihood of the data"""
 
 import torch
+import torch.nn.functional as F
 
 from scvi.utils import to_cuda, no_grad, eval_modules
 
@@ -8,7 +9,6 @@ from scvi.utils import to_cuda, no_grad, eval_modules
 @no_grad()
 @eval_modules()
 def compute_log_likelihood(vae, data_loader):
-    # Bad since depends on the sampler
     # Iterate once over the data_loader and computes the total log_likelihood
     log_lkl = 0
     for i_batch, tensors in enumerate(data_loader):
@@ -22,7 +22,9 @@ def compute_log_likelihood(vae, data_loader):
         log_lkl += torch.sum(reconst_loss).item()
     n_samples = (
         len(data_loader.dataset)
-        if data_loader.sampler is None
+        if not (
+            hasattr(data_loader, "sampler") and hasattr(data_loader.sampler, "indices")
+        )
         else len(data_loader.sampler.indices)
     )
     return log_lkl / n_samples
@@ -42,29 +44,25 @@ def log_zinb_positive(x, mu, theta, pi, eps=1e-8):
     eps: numerical stability constant
     """
 
-    def softplus(x):
-        return torch.log(1 + torch.exp(x))
-
     # theta is the dispersion rate. If .ndimension() == 1, it is shared for all cells (regardless of batch or labels)
     if theta.ndimension() == 1:
         theta = theta.view(
             1, theta.size(0)
         )  # In this case, we reshape theta for broadcasting
 
-    case_zero = softplus(
+    case_zero = F.softplus(
         (-pi + theta * torch.log(theta + eps) - theta * torch.log(theta + mu + eps))
-    )
-    -softplus(-pi)
+    ) - F.softplus(-pi)
 
     case_non_zero = (
         -pi
-        - softplus(-pi)
+        - F.softplus(-pi)
         + theta * torch.log(theta + eps)
         - theta * torch.log(theta + mu + eps)
         + x * torch.log(mu + eps)
         - x * torch.log(theta + mu + eps)
-        + torch.lgamma(x + theta)
-        - torch.lgamma(theta)
+        + torch.lgamma(x + theta + eps)
+        - torch.lgamma(theta + eps)
         - torch.lgamma(x + 1)
     )
 
