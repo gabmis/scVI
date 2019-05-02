52dbffdc786a7ff2fc410ad069cd8f296bc426eb
Valentine Svensson <v@nxn.se>
2019-05-02 14:59:01 -0700

    Linearly decoded VAE (#274)
    * First try of factor decoder, without batch effect handling.
    
    * Simple linear adjustment for batch effects (doesn't work well)
    
    * Made separate class for linearly decoded VAE
    
    Should mean less handling of special cases in VAE code.
    
    * Rename FactorDecoderSCVI to LinearDecoderSCVI
    
    The term 'factor' carries specific meaning in latent variable models.
    
    * Expose LDVAE model in scvi.models
    
    * Added LDVAE notebook
    
    * Added method to extract linear loadings
    And fixed flake errors
    
    * Added test for LDVAE
    
    * Test both single batch and two batches for LDVAE
    
    * Also call get_loadings in test to make sure it works
    
    * Using pbmc3k data for linear notebook
    
    * Added text to LDVAE notebook

--- scvi/models/__init__.py
+++ scvi/models/__init__.py
@@ -1,7 +1,7 @@
 from .classifier import Classifier
 from .scanvi import SCANVI
-from .vae import VAE
+from .vae import VAE, LDVAE
 from .vae_fish import VAEF
 from .vaec import VAEC
 
-__all__ = ["SCANVI", "VAEC", "VAE", "VAEF", "Classifier"]
+__all__ = ["SCANVI", "VAEC", "VAE", "LDVAE", "VAEF", "Classifier"]
